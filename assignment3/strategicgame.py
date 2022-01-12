1922c9d663c38e88d135389e0327e67bf1c56d96
katie-hofschen <katharina@hofschen.de>
2022-01-12 13:17:35 +0100

    Implemented IESDS and IENBR.

--- assignment3/strategicgame.py
+++ assignment3/strategicgame.py
@@ -85,44 +85,147 @@ class StrategicGame:
         else:
             raise Exception("The 2 actions you are comparing are of different lenghts.")
 
-    def get_dominatedActions(self, playerMat):
-        dominated = []
-        for combo in permutations(np.arange(0, self.n_action_rows), 2):
-            if self.strictlydominates(playerMat[combo[0]], playerMat[combo[1]]):
-                dominated.append(combo[1])
-                #print(dominated)
-        return dominated
+    def removeDominatedRowsCols(self, matrix, verbose=False):
+        dominated_rows = []
 
-    def get_remaining(self, playerUtil):
-        actionsToEliminate = self.get_dominatedActions(playerUtil)
-        remaining = [elem for elem in np.arange(0,self.n_action_rows) if elem not in actionsToEliminate]
-        return remaining
+        for combo in permutations(np.arange(0, matrix.shape[0]), 2):
+            if self.strictlydominates(matrix[combo[0], :, 0], matrix[combo[1], :, 0]):
+                if verbose:
+                    print("Removing row ", self.row_action_names[combo[1]],"(strictly dominated by ", self.row_action_names[combo[0]],")." )
+                dominated_rows.append(combo[1])
+        dominated_rows = sorted(dominated_rows, reverse=True)
+        for row in dominated_rows:
+            matrix = np.delete(matrix, row, axis=0)
+
+        dominated_cols = []
+        for combo in permutations(np.arange(0, matrix.shape[1]), 2):
+            if self.strictlydominates(matrix[:,combo[0],1], matrix[:,combo[1],1]):
+                if verbose:
+                    print("Removing column ", self.col_action_names[combo[1]],"(strictly dominated by ", self.col_action_names[combo[0]],")." )
+                dominated_cols.append(combo[1])
+        dominated_cols = sorted(dominated_cols, reverse=True)
+        for col in dominated_cols:
+            matrix = np.delete(matrix, col, axis=1)
+
+        return (matrix, dominated_rows, dominated_cols)
+
+    def remainingMatrix(self, remaining_rows, remaining_cols):
+        matrix = []
+        for rowID in remaining_rows:
+            row = []
+            for colID in remaining_cols:
+                row.append(self.matrix[rowID][colID])
+            matrix.append(row)
+        return matrix
+
+    def getRemainingNames(self, remaining, r_c=("R", "C")):
+        names = []
+        if r_c == "R":
+            for id in remaining:
+                names.append(self.row_action_names[id])
+        else:
+            for id in remaining:
+                names.append(self.col_action_names[id])
+        return names
 
     def iesds(self, verbose=False):
         '''Apply IESDS (Iteratrive Elimination) on the current StrategicGame and returns the resulting
         StrategicGame. Leaves the original StrategicGame unchanged.'''
-        displayMatrix = PrettyTable()
 
-        rowPlayerUtil = [[tup[0] for tup in row] for row in self.matrix]
-        remaining_rows = self.get_remaining(rowPlayerUtil)
-        remaining_matrix = [self.matrix[rowID] for rowID in remaining_rows]
+        tmp_matrix = np.array(self.matrix)
 
-        colPlayerUtil = np.array([[tup[1] for tup in row] for row in remaining_matrix]).T.tolist()
-        remaining_cols = self.get_remaining(colPlayerUtil)
+        tmp_result = self.removeDominatedRowsCols(tmp_matrix, verbose)
+        tmp_matrix_new = tmp_result[0]
+        dominated_rows = tmp_result[1]
+        dominated_cols = tmp_result[2]
 
-        first_row = [""] + [self.col_action_names[id] for id in remaining_cols]
-        displayMatrix.field_names = first_row
-        for rowID in remaining_rows:
-            row = [self.row_action_names[rowID]]
-            for colID in remaining_cols:
-                row.append(self.matrix[rowID][colID])
-            displayMatrix.add_row(row)
-        return str(displayMatrix)
+        while tmp_matrix.shape != tmp_matrix_new.shape:
+            tmp_matrix = tmp_matrix_new
+            tmp_result = self.removeDominatedRowsCols(tmp_matrix)
+            tmp_matrix_new = tmp_result[0]
+            dominated_rows = dominated_rows + tmp_result[1]
+            dominated_cols = dominated_cols + tmp_result[2]
+
+        remaining_rows = [elem for elem in np.arange(0,self.n_action_rows) if elem not in dominated_rows]
+        remaining_cols = [elem for elem in np.arange(0,self.n_action_cols) if elem not in dominated_cols]
+
+        resulting_matrix = self.remainingMatrix(remaining_rows,remaining_cols)
+        iesds_matrix = StrategicGame(resulting_matrix)
+        iesds_matrix.assign_row_actions_names(self.getRemainingNames(remaining_rows, "R"))
+        iesds_matrix.assign_col_actions_names(self.getRemainingNames(remaining_cols, "C"))
+        return iesds_matrix
+
+    def bestResponseToAction(self, matrix, actionID, r_c=("R", "C")):
+        if r_c == "R":
+            # the opponent is Row and takes action with actionID so we check what Cols best response to this is
+            best_response = max(matrix[actionID, :, 1])
+            max_index = np.where(matrix[actionID, :, 1] == best_response)
+        elif r_c == "C":
+            # the opponent is Column and takes action with actionID so we check what Rows best response to this is
+            best_response = max(matrix[:, actionID, 0])
+            max_index = np.where(matrix[:, actionID, 0]== best_response)
+        else:
+            raise Exception("The input of the function must specify whether an action is assumed by R or by C.")
+        return max_index
+
+    def neverBestResponse(self, matrix, r_c=("R", "C")):
+        best_response = []
+        if r_c == "R":
+            for row in range(0,matrix.shape[0]):
+                best_response += self.bestResponseToAction(matrix, row, r_c="R")
+            neverBest = [id for id in range(0, matrix.shape[0]) if id not in best_response]
+        elif r_c == "C":
+            for col in range(0, matrix.shape[1]):
+                best_response += self.bestResponseToAction(matrix, col, r_c="C")
+            neverBest = [id for id in range(0, matrix.shape[1]) if id not in best_response]
+        else:
+            raise Exception("The input of the function must specify whether an action is assumed by R or by C.")
+        return neverBest
+
+    def ienbr(self, verbose=False):
+        # non-destructively and iteratively eliminates never best responses
+        tmp_matrix = np.array(self.matrix)
+        tmp_matrix_old = np.array([])
+
+        removed_rows = []
+        removed_cols = []
+        while tmp_matrix.shape != tmp_matrix_old.shape:
+            tmp_matrix_old = tmp_matrix
+
+            neverbest_row = self.neverBestResponse(tmp_matrix_old, r_c="R")
+            removed_rows += neverbest_row
+            for id in sorted(neverbest_row, reverse=True):
+                if verbose:
+                    print("Removing row ", self.row_action_names[id], "because it is a never best response.")
+                tmp_matrix = np.delete(tmp_matrix, id, axis=0)
+
+            neverbest_col = self.neverBestResponse(tmp_matrix_old, r_c="C")
+            removed_cols += neverbest_col
+            for id in sorted(neverbest_col, reverse=True):
+                if verbose:
+                    print("Removing column ", self.col_action_names[id], "because it is a never best response.")
+                tmp_matrix = np.delete(tmp_matrix, id, axis=1)
+
+        remaining_rows = [elem for elem in np.arange(0,tmp_matrix.shape[0]) if elem not in list(set(removed_rows))]
+        remaining_cols = [elem for elem in np.arange(0,tmp_matrix.shape[1]) if elem not in list(set(removed_cols))]
+
+        resulting_matrix = self.remainingMatrix(remaining_rows ,remaining_cols)
+        ienbr_matrix = StrategicGame(resulting_matrix)
+        ienbr_matrix.assign_row_actions_names(self.getRemainingNames(remaining_rows, "R"))
+        ienbr_matrix.assign_col_actions_names(self.getRemainingNames(remaining_cols, "C"))
+        return ienbr_matrix
+
 
     def find_Nash_profiles(self):
         '''return the list of Nash equilibria, as a list of row action /
            column action pairs (aidr, aidc).'''
         pass
+
+    def find_dominant_strategy_profiles(self):
+        # returns a list of dominant profiles as a list of row action/ column action ID pairs
+        # a profile is a dominant profile if every player's action is a (strongly) dominant strategy
+        pass
+
 
 
 def main():
@@ -150,14 +253,19 @@ def main():
     m3.assign_row_actions_names(["rock", "paper", "scissors"])
     m3.assign_col_actions_names(["rock", "paper", "scissors"])
 
+    # print(f"Before IESDS\n{m3}")
+
     m4 = StrategicGame([[(1, 4), (2, 4)],
                         [(1, 10), (4, 0)],
                         [(0, 10), (8, 11)]])
     for m in [m0, m1, m2, m3, m4]:
         print("Nash equilibria:", m.find_Nash_profiles())
 
+    m5 = StrategicGame([[(2,1), (0,0)],
+                        [(0,1),(2,0)],
+                        [(1,1),(1,2)]])
 
-    # pass
+    print(m5.ienbr(verbose=True))
 
 
 if __name__ == '__main__':
